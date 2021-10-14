999e3dfe4096bb9f9f4334f427af16c930b78e8b
Javi <fmargon004a@ieslacampina.es>
2021-10-14 10:03:21 +0200

    Fichero con update

--- update.sql
+++ update.sql
@@ -1,1 +1,15 @@
-update
+UPDATE usuario1
+SET apellido = 'RODRIGUEZ'
+WHERE nombre = 'ANTONIO';
+
+UPDATE aula2
+SET numero = 3
+WHERE numero = 1;
+
+UPDATE curso3
+SET nombre = 'IAW'
+WHERE nombre = 'Fundamentos de Hardware';
+
+UPDATE edificio4
+SET numero = 4
+WHERE numero = 1;

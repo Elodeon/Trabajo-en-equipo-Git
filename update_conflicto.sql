750c580718038f247aeeeff634c5f22f9012b026
Javi <fmargon004a@ieslacampina.es>
2021-10-21 09:17:04 +0200

    Conflicto

---
+++ update_conflicto.sql
@@ -0,0 +1,17 @@
+UPDATE usuario1
+SET apellido = 'RODRIGUEZ'
+WHERE nombre = 'ANTONIO';
+
+UPDATE aula2
+SET numero = 3
+WHERE numero = 1;
+
+UPDATE curso3
+SET nombre = 'IAW'
+WHERE nombre = 'Fundamentos de Hardware';
+
+UPDATE edificio4
+SET numero = 4
+WHERE numero = 1;
+
+/* Esto va a suponer un conflicto */
